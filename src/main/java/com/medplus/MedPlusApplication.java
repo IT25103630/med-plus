41748c05453a127de40e109ded75a7cd7d161a30
NaveenChathurya <naveenchathurya2@gmail.com>
2026-05-17 02:44:36 +0530

    Add initial project

---
+++ src/main/java/com/medplus/MedPlusApplication.java
@@ -0,0 +1,14 @@
+package com.medplus;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPlusApplication.class, args);
+	}
+}
+
